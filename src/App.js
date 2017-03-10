472153aa8216c030f8b4d233d57d208f565676d8
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-03-10 02:31:57 -0500

    style and Box-reveal removal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import BoxReveal from './components/BoxReveal';
+import React from 'react';
+import './css/App.css';
+
 import Blotty from './components/Blotty';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <BoxReveal  delay={.5} duration={1}
-                      color={'#fff'} name="header">
-            <h2>React Loader Animations</h2>
-          </BoxReveal>
-        </div>
-        <Blotty/>
-      </div>
-    );
-  }
-}
+const App = () => (
+
+  <div className="App">
+    <div className="App-header">
+        <h2>React Loader Animations</h2>
+    </div>
+
+
+      {/* LOADERS */}
+    <div className="loaders">
+      <Blotty/>
+
+    </div>
+
+  </div>
+
+)
 
 export default App;
