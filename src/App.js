07cd68aca0d8f5d9b34cca83abd49a7f8400e2ea
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-03-12 11:51:12 -0400

    renderCreditDetail for inspired & credited props (#2)

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ const App = () => (
     {/* LOADERS */}
     <div className="loaders">
       <Loader name="GSpinner" credit="Christian Eckenrode" creditLinkText="g-spinner"
-              creditLink="https://github.com/ceckenrode/g-spinner">
+              creditLink="https://github.com/ceckenrode/g-spinner" credited>
 
         {/* G Spinner */}
         <GSpinner/>
 
       </Loader>
       <Loader name="Blotty" credit="Joe Harry" creditLinkText="Samsung TV Loader"
-              creditLink="https://codepen.io/woodwork/pen/YWjWzo">
+              creditLink="https://codepen.io/woodwork/pen/YWjWzo" inspired>
 
         {/* Blotty */}
         <Blotty/>
