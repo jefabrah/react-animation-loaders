78e47527c131a53512f465168a0c769119078c48
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-03-26 21:36:36 -0400

    Radium refactor (#6)
    * rename to LoaderDetail & Radium StyleRoot
    
    * converted to radium
    
    * remove Component destructuring from react import
    
    * radium
    
    * dev radium wrapper config
    
    * radium refactor
    
    * animation name fix
    
    * radium refactor
    
    * radium refactor
    
    * loaderDetail fix

--- src/components/Pulse.js
+++ src/components/Pulse.js
@@ -1,11 +1,61 @@
 import React from 'react';
-import '../css/Pulse.css';
+import ConfiguredRadium from '../ConfiguredRadium';
+import Radium from 'radium';
 
 
-const Pulse = () => (
-  <div id="pulse-box">
-    <div id="pulse" />
-  </div>
-)
+const Pulse = () => {
 
-export default Pulse;
+  const pulseKeyframes = Radium.keyframes({
+    '0%, 10%': { background: '#efefef' },
+    '50%': { background: '#5b5b5b' }
+  }, 'pulse');
+
+  const pulseBoxStyles = {
+    width: '100%',
+    height: '100px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+  const pulseLeftStyles = {
+    width: '12.5px',
+    height: '33.3px',
+    marginRight: '10px',
+    background: '#efefef',
+    animation: 'x 750ms infinite',
+    animationName: pulseKeyframes
+  }
+
+  const pulseCenterStyles = {
+    width: '12.5px',
+    height: '50px',
+    animation: 'x 750ms infinite',
+    animationName: pulseKeyframes,
+    animationDelay: '250ms',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+  const pulseRightStyles = {
+    width: '12.5px',
+    height: '33.3px',
+    marginLeft: '10px',
+    background: '#efefef',
+    animation: 'x 750ms infinite',
+    animationName: pulseKeyframes,
+    animationDelay: '500ms'
+  }
+  
+
+  return (
+    <div style={pulseBoxStyles} >
+      <div style={pulseLeftStyles} />
+      <div style={pulseCenterStyles} />
+      <div style={pulseRightStyles} />
+    </div>
+  )
+}
+
+export default ConfiguredRadium(Pulse);
