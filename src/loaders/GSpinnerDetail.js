797f8e29ef60073ea5cf169e843ac0cfd7b2df63
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-04-09 03:29:51 -0400

    selectable components (#9)

---
+++ src/loaders/GSpinnerDetail.js
@@ -0,0 +1,229 @@
+import React from 'react';
+import ConfiguredRadium from '../ConfiguredRadium';
+import Radium from 'radium';
+
+
+const GSpinnerDetail = ({ loading, children }) => {
+
+  // ANIMATIONS
+  const gSpinnerBoxRotate = Radium.keyframes({
+    from: { transform: 'rotate(0deg)' },
+    to: { transform: 'rotate(360deg)' }
+  }, 'gSpinnerBoxRotate')
+
+  const gSpinnerC1Animation = Radium.keyframes({
+    '0%': {
+      background: '#4384F8',
+      transform: 'translateY(-80px)',
+    },
+    '24%': {
+      background: '#4384F8',
+    },
+    '25%': {
+      background: '#FBBD06',
+      transform: 'translateY(0px)',
+    },
+    '50%': {
+      background: '#FBBD06',
+      transform: 'translateY(-80px)',
+    },
+    '55%': {
+      background: '#FBBD06',
+      transform: 'translateY(-80px)',
+    },
+    '74%': {
+      background: '#FBBD06',
+    },
+    '75%': {
+      background: '#4384F8',
+      transform: 'translateY(0px)',
+    },
+    '100%': {
+      background: '#4384F8',
+      transform: 'translateY(-80px)'
+    }
+  }, 'gSpinnerC1Animation');
+
+  const gSpinnerC2Animation = Radium.keyframes({
+    '0%': {
+      background: '#34A855',
+      transform: 'translateX(-80px)',
+    },
+    '24%': {
+      background: '#34A855',
+    },
+    '25%': {
+      background: '#EA4436',
+      transform: 'translateX(0px)',
+    },
+    '50%': {
+      background: '#EA4436',
+      transform: 'translateX(-80px)',
+    },
+    '55%': {
+      background: '#EA4436',
+      transform: 'translateX(-80px)',
+    },
+    '74%': {
+      background: '#EA4436',
+    },
+    '75%': {
+      background: '#34A855',
+      transform: 'translateX(0px)',
+    },
+    '100%': {
+      background: '#34A855',
+      transform: 'translateX(-80px)'
+    }
+  }, 'gSpinnerC2Animation');
+
+  const gSpinnerC3Animation = Radium.keyframes({
+    '0%': {
+      background: '#FBBD06',
+      transform: 'translateY(80px)',
+    },
+    '24%': {
+      background: '#FBBD06',
+    },
+    '25%': {
+      background: '#4384F8',
+      transform: 'translateY(0px)',
+    },
+    '50%': {
+      background: '#4384F8',
+      transform: 'translateY(80px)',
+    },
+    '55%': {
+      background: '#4384F8',
+      transform: 'translateY(80px)',
+    },
+    '74%': {
+      background: '#4384F8',
+    },
+    '75%': {
+      background: '#FBBD06',
+      transform: 'translateY(0px)',
+    },
+    '100%': {
+      background: '#FBBD06',
+      transform: 'translateY(80px)'
+    }
+  }, 'gSpinnerC3Animation');
+
+  const gSpinnerC4Animation = Radium.keyframes({
+    '0%': {
+      background: '#EA4436',
+      transform: 'translateX(80px)',
+    },
+    '24%': {
+      background: '#EA4436',
+    },
+    '25%': {
+      background: '#34A855',
+      transform: 'translateX(0px)',
+    },
+    '50%': {
+      background: '#34A855',
+      transform: 'translateX(80px)',
+    },
+    '55%': {
+      background: '#34A855',
+      transform: 'translateX(80px)',
+    },
+    '74%': {
+      background: '#34A855',
+    },
+    '75%': {
+      background: '#EA4436',
+      transform: 'translateX(0px)',
+    },
+    '100%': {
+      background: '#EA4436',
+      transform: 'translateX(80px)'
+    }
+  }, 'gSpinnerC4Animation');
+
+
+  // STYLING
+  const gSpinnerWrapperStyles = {
+    position: 'relative',
+    width: '100%',
+    height: '220px',
+  }
+
+  const gSpinnerBoxStyles = {
+    position: 'relative',
+    margin: '0 auto',
+    width: '220px',
+    height: '220px',
+    animation: 'x 2s cubic-bezier(0.465, 0.183, 0.153, 0.946) infinite',
+    animationName: gSpinnerBoxRotate
+  }
+
+  const gSpinnerC1 = {
+    
+    height: '40px',
+    width: '40px',
+    position: 'absolute',
+    borderRadius: '50%',
+    background: '#4384F8',
+    top: 'calc(50% - 20px)',
+    left: 'calc(50% - 20px)',
+    animation: 'x 4s cubic-bezier(0.645, 0.045, 0.355, 1) infinite',
+    animationName: gSpinnerC1Animation
+  }
+
+  const gSpinnerC2 = {
+    
+    height: '40px',
+    width: '40px',
+    position: 'absolute',
+    borderRadius: '50%',
+    background: '#4384F8',
+    top: 'calc(50% - 20px)',
+    left: 'calc(50% - 20px)',    
+    animation: 'x 4s cubic-bezier(0.645, 0.045, 0.355, 1) infinite',
+    animationName: gSpinnerC2Animation
+  }
+
+  const gSpinnerC3 = {
+    
+    height: '40px',
+    width: '40px',
+    position: 'absolute',
+    borderRadius: '50%',
+    background: '#4384F8',
+    top: 'calc(50% - 20px)',
+    left: 'calc(50% - 20px)',    
+    animation: 'x 4s cubic-bezier(0.645, 0.045, 0.355, 1) infinite',
+    animationName: gSpinnerC3Animation
+  }
+
+  const gSpinnerC4 = {
+    
+    height: '40px',
+    width: '40px',
+    position: 'absolute',
+    borderRadius: '50%',
+    background: '#4384F8',
+    top: 'calc(50% - 20px)',
+    left: 'calc(50% - 20px)',    
+    animation: 'x 4s cubic-bezier(0.645, 0.045, 0.355, 1) infinite',
+    animationName: gSpinnerC4Animation
+  }
+  console.log(typeof children)
+  return loading ? (
+
+    <div style={gSpinnerWrapperStyles}>
+      <div style={gSpinnerBoxStyles}>
+        <div style={gSpinnerC1} />
+        <div style={gSpinnerC2} />
+        <div style={gSpinnerC3} />
+        <div style={gSpinnerC4} />
+      </div>
+    </div>
+
+  ) : children
+}
+
+export default ConfiguredRadium(GSpinnerDetail);
--- src/loaders/Pulse.js
+++ src/loaders/Pulse.js
@@ -1,63 +1,15 @@
 import React from 'react';
-import ConfiguredRadium from '../ConfiguredRadium';
-import Radium from 'radium';
+import { StyleRoot } from 'radium';
+import PulseDetail from './PulseDetail';
 
+const Pulse = ({ loading, children }) => (
 
-const Pulse = ({ loading, children }) => {
+  <StyleRoot>
+    <PulseDetail loading={loading}>
+      {children}
+    </PulseDetail>
+  </StyleRoot>
 
-  const pulseKeyframes = Radium.keyframes({
-    '0%, 10%': { background: '#efefef' },
-    '50%': { background: '#5b5b5b' }
-  }, 'pulse');
+)
 
-  const pulseBoxStyles = {
-    width: '100%',
-    height: '100px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-
-  const pulseLeftStyles = {
-    width: '12.5px',
-    height: '33.3px',
-    marginRight: '10px',
-    background: '#efefef',
-    animation: 'x 750ms infinite',
-    animationName: pulseKeyframes
-  }
-
-  const pulseCenterStyles = {
-    width: '12.5px',
-    height: '50px',
-    animation: 'x 750ms infinite',
-    animationName: pulseKeyframes,
-    animationDelay: '250ms',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-
-  const pulseRightStyles = {
-    width: '12.5px',
-    height: '33.3px',
-    marginLeft: '10px',
-    background: '#efefef',
-    animation: 'x 750ms infinite',
-    animationName: pulseKeyframes,
-    animationDelay: '500ms'
-  }
-  
-
-  return loading ? (
-
-    <div style={pulseBoxStyles} >
-      <div style={pulseLeftStyles} />
-      <div style={pulseCenterStyles} />
-      <div style={pulseRightStyles} />
-    </div>
-    
-  ) : children
-}
-
-export default ConfiguredRadium(Pulse);
+export default Pulse;
