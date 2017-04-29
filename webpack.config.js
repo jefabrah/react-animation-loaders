bc298526affb129bd6fe04723b64a345f716662b
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-04-29 17:01:40 -0400

    GSpinner fix

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     }
   ],
   output: {
-    path: path.join(__dirname, '/lib'),
+    path: path.join(__dirname, '/'),
     filename: 'index.js',
     library: 'ReactAnimationLoaders',
     libraryTarget: 'umd'
