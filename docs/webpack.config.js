68917a97ee82a4cbae1a80968b8b07ceba703d0e
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-05-08 00:29:41 -0400

    dev/demo config refactor (#22)
    * dev server fix and script cleanup
    
    * remove webpack-dashboard & script refactor

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const loaders = require('./webpack.loaders');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "8888";
@@ -15,71 +14,40 @@ loaders.push({
   exclude: ['node_modules']
 });
 
-module.exports = [
-  {
-    cache: true,
-    entry: '../loaders/index.js',
-    externals: [
-      {
-        "react": {
-          root: "React",
-          commonjs2: "react",
-          commonjs: "react",
-          amd: "react"
-        }
-      }
-    ],
-    output: {
-      path: path.join(__dirname, 'lib'),
-      filename: 'index.js',
-      library: 'ReactAnimationLoaders',
-      libraryTarget: 'umd'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader'
-        }
-      ]
-    }
+module.exports = {
+  cache: true,
+  entry: [
+    './src/index.js',
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+  output: {
+    filename: 'demo_bundle.js'
   },
-  {
-    entry: [
-      './src/index.js',
-    ],
-    devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
-    output: {
-      publicPath: '/',
-      filename: 'main.js'
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    module: {
-      loaders
-    },
-    devServer: {
-      contentBase: "./",
-      // do not print bundle build stats
-      noInfo: true,
-      // enable HMR
-      hot: true,
-      // embed the webpack-dev-server runtime into the bundle
-      inline: true,
-      // serve index.html in place of 404 responses to allow HTML5 history
-      historyApiFallback: true,
-      port: PORT,
-      host: HOST
-    },
-    plugins: [
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new ExtractTextPlugin({
-        filename: 'style.css',
-        allChunks: true
-      }),
-      new DashboardPlugin(),
-    ]
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-]
+  module: {
+    loaders
+  },
+  devServer: {
+    contentBase: "./",
+    // do not print bundle build stats
+    noInfo: true,
+    // enable HMR
+    hot: true,
+    // embed the webpack-dev-server runtime into the bundle
+    inline: true,
+    // serve index.html in place of 404 responses to allow HTML5 history
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'demo_bundle.css',
+      allChunks: true
+    }),
+  ]
+}
